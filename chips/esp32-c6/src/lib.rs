959c78cba66ca5f389c9eca436a0a9c49e089d02
Bobby Reynolds <bobby@reynoldsbd.net>
2024-11-24 15:34:29 -0800

    [wip] esp32-c6: Initial Chip Crate

---
+++ chips/esp32-c6/src/lib.rs
@@ -0,0 +1,8 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2022.
+
+#![no_std]
+
+pub mod chip;
+pub mod rom;
